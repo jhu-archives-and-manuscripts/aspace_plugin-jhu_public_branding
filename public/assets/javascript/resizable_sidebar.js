7ef5faefc096a114e8b03903aced7f2fe9836932
Tim DiLauro <tim.di.lauro@gmail.com>
2018-11-16 22:45:13 -0500

    ambidextrous resizable sidebar

--- public/assets/javascript/resizable_sidebar.js
+++ public/assets/javascript/resizable_sidebar.js
@@ -8,13 +8,13 @@ function ResizableSidebar($sidebar) {
         // only do things if there's a content pane and a sidebar
         this.$sidebar.removeClass('resizable-sidebar');
         return;
-    };
+    }
 
     this.$row.addClass('has-resizable-content');
 
     this.add_handle();
     this.bind_events();
-};
+}
 
 ResizableSidebar.prototype.add_handle = function() {
     var $handle = $('<a>').attr('href', 'javascript:void(0);');
@@ -30,6 +30,23 @@ ResizableSidebar.prototype.add_handle = function() {
 ResizableSidebar.prototype.bind_events = function() {
     var self = this;
 
+    var min_sidebar_width = 200;
+    var min_content_width = 500;
+    var i_scrollbar_width = 20;
+
+    var content_left = {
+    			leftObject: self.$content_pane,
+          rightObject: self.$sidebar,
+          min_left_width: min_content_width,
+          min_right_width: min_sidebar_width,
+		}
+    var content_right = {
+    	    leftObject: self.$sidebar,
+          rightObject: self.$content_pane,
+          min_left_width: min_sidebar_width,
+          min_right_width: min_content_width,
+    }
+
     self.$handle.on('mousedown', function (e) {
         self.isResizing = true;
         self.lastDownX = e.clientX;
@@ -40,24 +57,32 @@ ResizableSidebar.prototype.bind_events = function() {
             return;
         }
 
+        // content or sidebar on the left?
+        if (self.$content_pane.offset().left < self.$sidebar.offset().left) {
+        	object = content_left;
+        } else {
+        	object = content_right;
+        }
+
+        leftObject = object.leftObject;
+        rightObject = object.rightObject;
+        min_left_width = object.min_left_width;
+        min_right_width = object.min_right_width;
+
         var cursor_x = e.clientX;
+        var left_width = cursor_x - self.$row.offset().left;
+        left_width = Math.max(left_width, min_left_width);
+        left_width = Math.min(left_width, self.$row.width() - min_right_width);
+        var right_width = self.$row.width() - left_width;
 
-        var sidebar_min_width = 200;
-        var content_min_width = 300;
-
-        // if it's on the right side
-        var right_offset = (self.$row.width() + self.$row.offset().left) - cursor_x;
-
-        right_offset = Math.max(right_offset, sidebar_min_width);
-        var new_content_width = Math.max(self.$row.width() - right_offset, content_min_width);
-
-        self.$sidebar.css('width', 0);
-        self.$content_pane.css('width', new_content_width);
-        self.$sidebar.css('width', self.$row.width() - new_content_width);
+        rightObject.css('width', 0);
+        leftObject.css('width', left_width);
+        rightObject.css('width', right_width);
 
         // position the infinite scrollbar too, if it's about
         if ($('.infinite-record-scrollbar').length > 0) {
-            $('.infinite-record-scrollbar').css('left', self.$row.offset().left + new_content_width - 20);
+            // $('.infinite-record-scrollbar').css('left', self.$row.offset().left + new_content_width - i_scrollbar_width);
+            $('.infinite-record-scrollbar').css('left', self.$row.offset().left + left_width);
         }
     }).on('mouseup', function (e) {
         self.isResizing = false;
